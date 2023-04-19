1982e6bc108136fbcc520237bade7c7fedf67900
guilhermefaj <figueiredoguiu@gmail.com>
2023-04-19 10:50:03 -0300

    Refactor: Alteração de porta

--- src/app.js
+++ src/app.js
@@ -122,5 +122,5 @@ app.get("/home", async (req, res) => {
     }
 })
 
-const PORT = 5000
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
